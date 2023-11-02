ebaf5686a682265b6795dac52a78e9b44940d71d
Eugeneche <eugene.bizmail@gmail.com>
2023-11-02 22:47:06 +0100

    fixed stripejs

--- src/utils/stripejs.js
+++ src/utils/stripejs.js
@@ -1,14 +1,9 @@
 import { loadStripe } from "@stripe/stripe-js"
 
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 let stripePromise
 const getStripe = () => {
-  console.log(process.env.STRIPE_PUBLISHABLE_KEY)
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY)
+    stripePromise = loadStripe(process.env.GATSBY_STRIPE_PUBLISHABLE_KEY)
   }
   return stripePromise
 }
