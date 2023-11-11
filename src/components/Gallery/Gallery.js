6890d9bc372cdc72aed1a5c65e2fe0c1c348ff59
Eugeneche <eugene.bizmail@gmail.com>
2023-11-11 15:56:15 +0100

    Designed Magnifier

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,8 +1,11 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-import { GatsbyImage } from "gatsby-plugin-image"
+//import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import useTranslations from "../useTranslations"
 import * as styles from "./_Gallery.module.scss"
+import Magnifier from "../Magnifier/Magnifier"
+
+//import i from "../../images/gallery/1.jpg"
 
 const Gallery = () => {
 
@@ -11,34 +14,32 @@ const Gallery = () => {
         } = useTranslations()
 
     const data = useStaticQuery(graphql`
-        query getGallery {
-            allFile(filter: {sourceInstanceName: {eq: "gallery"}}) {
-                nodes {
-                    childImageSharp {
-                        gatsbyImageData
-                        id
-                    }
-                    id
-                }
+    query getGallery {
+        allFile(filter: {sourceInstanceName: {eq: "gallery"}}) {
+          nodes {
+            childImageSharp {
+              gatsbyImageData
+              id
             }
+            id
+          }
         }
+      }
     `)
 
     const images = data.allFile.nodes
-    //console.log(images)
 
     return (
         <section className={styles.section}>
             <div className={styles.container}>
                 <h2>{gallery}</h2>
                 <div className={styles.galleryWrapper}>
+                
                     {images.map(img => {
                         return (
-                            <GatsbyImage
-                                key={img.childImageSharp.id}
-                                image={img.childImageSharp.gatsbyImageData}
-                                alt="women's jacket image"
-                            />
+                            
+                            <Magnifier key={img.childImageSharp.id} image={img.childImageSharp.gatsbyImageData.images.fallback.src} />                               
+                            
                         )
                     })}
                 </div>
